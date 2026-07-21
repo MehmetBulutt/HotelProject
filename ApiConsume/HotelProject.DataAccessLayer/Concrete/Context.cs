4289c7fc3b4953d9adbb0b62cd12510258a6642e
MehmetBulutt <mehmetbulut7176@gmail.com>
2026-07-21 12:10:13 +0300

    feat: initialize hotel management project

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        override
+            protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Mehmet-Bulut\\SQLEXPRESS; initial catalog=ApiDb;integrated security=true");
+        }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
