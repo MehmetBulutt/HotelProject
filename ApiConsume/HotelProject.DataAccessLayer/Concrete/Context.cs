0ca9cd761cd0dda6a9fb0b3bdfcd8b95aaa15e6c
MehmetBulutt <mehmetbulut7176@gmail.com>
2026-07-21 14:45:22 +0300

    feat: add hotel management API endpoints

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HotelProject.EntityLayer.Concrete;
+﻿using HotelProject.EntityLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelProject.DataAccessLayer.Concrete
 {
-    public class Context:DbContext
+    public class Context : DbContext
     {
-        override
-            protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public Context(DbContextOptions<Context> options)
+            : base(options)
         {
-            optionsBuilder.UseSqlServer(
-    "Server=Mehmet-Bulut\\SQLEXPRESS;Database=ApiDb;Trusted_Connection=True;TrustServerCertificate=True;");
         }
+
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staffs { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<Subscribe> Subscribes { get; set; }
-
-
     }
 }
--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -1,9 +1,36 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(
+        "Server=Mehmet-Bulut\\SQLEXPRESS;Database=ApiDb;Trusted_Connection=True;TrustServerCertificate=True;");
+});
+
+// Business Layer
+builder.Services.AddScoped<IRoomService, RoomManager>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+// Data Access Layer
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
